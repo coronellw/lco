877e8311aaaab149f64de35ebda4f1a7fefe9d9f
Winston Coronell <coronell@gmail.com>
2014-02-09 23:27:38 -0600

    secret_token fix

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,19 +9,20 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-require 'securerandom'
 
-def secure_token
-	token_file = Rails.root.join('.secret')
-	if File.exist?(token_file)
-		#Use the existing token
-		File.read(token_file).chomp
-	else
-		#generate a new token
-		token = SecureRandom.hex(64)
-		File.write(token_file, token)
-	end
-end
+#require 'securerandom'
 
-LCO::Application.config.secret_key_base = secure_token
-#LCO::Application.config.secret_key_base = '4022c50732f988a214a2c5838b03616ebb05663d3597b48593873140290b237239db91bb5c46d3fce20938c53bbc0462e75fde18d01faf2ea715ffc664b76ae9'
+#def secure_token
+#	token_file = Rails.root.join('.secret')
+#	if File.exist?(token_file)
+#		#Use the existing token
+#		File.read(token_file).chomp
+#	else
+#		#generate a new token
+#		token = SecureRandom.hex(64)
+#		File.write(token_file, token)
+#	end
+#end
+
+#LCO::Application.config.secret_key_base = secure_token
+LCO::Application.config.secret_key_base = '4022c50732f988a214a2c5838b03616ebb05663d3597b48593873140290b237239db91bb5c46d3fce20938c53bbc0462e75fde18d01faf2ea715ffc664b76ae9'
